cba6b0bf1961013bf24bafe385f68a5423ec8d87
Veera-devp <saikam.raghava@gmail.com>
2024-01-01 22:05:23 +0530

    Authorization Bug fixed

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,14 +1,44 @@
-import React from "react";
-import { Form, Input, Button } from "antd";
+import React, { useEffect } from "react";
+import { Form, Input, Button, message } from "antd";
 import { Link } from "react-router-dom";
 import Divider from "../../components/Divider";
-function Index() {
+import { LoginUser } from "../../apicalls/users";
+import { useDispatch, useSelector } from "react-redux";
+import { SetButtonLoading } from "../../redux/loadersSlice";
+import { getAntdFormInputRules } from "../../utils/helpers";
+
+function Login() {
+  const { buttonLoading } = useSelector((state) => state.loaders);
+  const dispatch = useDispatch();
+  const onFinish = async (values) => {
+    try {
+      dispatch(SetButtonLoading(true));
+      const response = await LoginUser(values);
+      dispatch(SetButtonLoading(false));
+      if (response.success) {
+        localStorage.setItem("token", response.data);
+        message.success(response.message);
+        window.location.href = "/";
+      } else {
+        throw new Error(response.message);
+      }
+    } catch (error) {
+      dispatch(SetButtonLoading(false));
+      message.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      window.location.href = "/";
+    }
+  }, []);
 
   return (
     <div className="grid grid-cols-2">
       <div className="bg-primary h-screen flex flex-col justify-center items-center">
         <div>
-          <h1 className="text-7xl text-white">TASK HARBOR</h1>
+          <h1 className="text-7xl text-white">SHEY-TRACKER</h1>
           <span className=" text-white mt-5">
             One place to track all your business records
           </span>
@@ -18,16 +48,18 @@ function Index() {
         <div className="w-[420px]">
           <h1 className="text-2xl text-gray-700">LOGIN TO YOUR ACCOUNT</h1>
           <Divider />
-          <Form layout="vertical">
+          <Form layout="vertical" onFinish={onFinish}>
             <Form.Item
               label="Email"
               name="email"
+              rules={getAntdFormInputRules}
             >
               <Input />
             </Form.Item>
             <Form.Item
               label="Password"
               name="password"
+              rules={getAntdFormInputRules}
             >
               <Input type="password" />
             </Form.Item>
@@ -36,8 +68,9 @@ function Index() {
               type="primary"
               htmlType="submit"
               block
+              loading={buttonLoading}
             >
-              Login
+              {buttonLoading ? "Loading" : "Login"}
             </Button>
 
             <div className="flex justify-center mt-5">
@@ -49,8 +82,7 @@ function Index() {
         </div>
       </div>
     </div>
-
   );
 }
 
-export default Index;
+export default Login;
